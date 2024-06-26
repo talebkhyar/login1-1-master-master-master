04119970837ebb2b721537d756f7cc7363e6de45
talebkhyar <tkcans33@gmail.com>
2024-06-26 20:50:50 +0000

    correction erreur

--- lib/pages/afficheReclemation.dart
+++ lib/pages/afficheReclemation.dart
@@ -14,156 +14,33 @@ class _AfficheReclemationState extends State<AfficheReclemation> {
   String? emailUser;
   bool isLoading = true;
   List<QueryDocumentSnapshot> datas = [];
-  getData() async {
-    QuerySnapshot querySnapshot =
-        await FirebaseFirestore.instance.collection('reclemations').get();
-    datas.addAll(querySnapshot.docs);
-  }
-
-  // List<Map<String, dynamic>> studentsData = [];
-  // var _fullNameController;
-  // final credential = FirebaseAuth.instance.currentUser;
-  // getPdfUrlsFromFirestore() async {
-  //   try {
-  //     CollectionReference pdfCollection =
-  //         FirebaseFirestore.instance.collection('reclemations');
-  //     QuerySnapshot querySnapshot = await pdfCollection.get();
-  //     DocumentSnapshot studentDoc =
-  //         await pdfCollection.doc(credential!.uid).get();
-  //     querySnapshot.docs.forEach((doc) {
-  //       studentsData.add(studentDoc.data() as Map<String, dynamic>);
-
-  //       _fullNameController.text = studentDoc['full_name'] ?? '';
-  //       print(_fullNameController);
-  //     });
-  //   } catch (e) {
-  //     print('Erreur lors de la récupération des URLs des PDF: $e');
-  //     // Rejette l'erreur pour la capturer dans catchError
-  //   }
-  // }
 
   @override
   void initState() {
-    getData();
-    getDataFromFirestore();
-    isLoading = false;
     super.initState();
+    getDataFromFirestore();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          title: const Text('Reclamation Envoié'),
-          centerTitle: true,
-        ),
-        body: datas.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          :
-        ListView.builder(
-            itemCount: datas.length,
-            itemBuilder: (context, i) {
-              if (emailUser == datas[i]['email']) {
-                return Padding(
-                  padding: const EdgeInsets.only(top: 5, right: 10, left: 10),
-                  child: Card(
-                      child: ListTile(
-                         onTap: () {
-              // Action à effectuer lors du clic sur un élément de la liste
-              // Par exemple, afficher un dialogue avec les détails de la réclamation
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text("Détails de la réclamation"),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                      
-                        Text('${datas[i]['details']}',
-                        style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),),
-                      ],
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        child: Text("Fermer"),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-                    leading: Text((i + 1).toString()),
-                    title: Column(
-                      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          children: [
-                            Text(datas[i]['full_name']),
-                            Text('Tu as poster une reclamation'),
-                          ],
-                        ),
-                        Text(
-                            'Semestre Reclamer  :  ' + datas[i]['semester']),
-                        Text('Nom Du Matiere    : ' + datas[i]['nomMatiere']),
-                        //if (datas[i]['reclemationDeExamen'] == 'Non')
-                        Text('La Note Exact        : ' + datas[i]['noteExact']),
-                        // if (datas[i]['reclemationDeExamen'] == 'Oui') Text(''),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(child: Text('Envoyée le : ')),
-                            Expanded(child: Text(datas[i]['dateEnvoie'])),
-                          ],
-                        ),
-                        if (datas[i]['etat'] == 'Envoyée')
-                          Container(
-                              padding: EdgeInsets.all(6),
-                              decoration: const BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: Text(
-                                  'La Reclamation est : ' + datas[i]['etat'])),
-                        if (datas[i]['etat'] == 'Refusée')
-                          Container(
-                              padding: EdgeInsets.all(6),
-                              decoration: const BoxDecoration(
-                                  color: Colors.redAccent,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: Text(
-                                  'La Reclamation est : ' + datas[i]['etat'])),
-                        if (datas[i]['etat'] == 'Acceptée')
-                          Container(
-                              padding: EdgeInsets.all(6),
-                              decoration: const BoxDecoration(
-                                  color: Color.fromARGB(255, 134, 255, 82),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: Text(
-                                  'La Reclamation est : ' + datas[i]['etat'])),
-                        if (datas[i]['etat'] == 'Révisée')
-                          Container(
-                              padding: EdgeInsets.all(6),
-                              decoration: const BoxDecoration(
-                                  color: Colors.blueAccent,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: Text(
-                                  'La Reclamation est : ' + datas[i]['etat'])),
-                      ],
-                    ),
-                  )),
-                );
-              }
-            }));
+  Future<void> getData() async {
+    if (emailUser == null) {
+      return;
+    }
+
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('reclemations')
+        .where('email', isEqualTo: emailUser)
+        .get();
+    print('Number of documents retrieved: ${querySnapshot.docs.length}');
+
+    querySnapshot.docs.forEach((doc) {
+      print('Document data: ${doc.data()}');
+    });
+
+    setState(() {
+      datas.clear();
+      datas.addAll(querySnapshot.docs);
+      isLoading = false;
+    });
   }
 
   Future<void> getDataFromFirestore() async {
@@ -174,41 +51,137 @@ class _AfficheReclemationState extends State<AfficheReclemation> {
         return;
       }
 
-      // Récupérer la référence de la collection principale
       CollectionReference etudiantsCollection =
           FirebaseFirestore.instance.collection('etudiants');
 
-      // Récupérer tous les documents de la collection principale
       QuerySnapshot querySnapshot = await etudiantsCollection.get();
 
-      // Parcourir tous les documents
-      // Parcourir tous les documents
       for (var doc in querySnapshot.docs) {
-        // Récupérer la sous-collection avec l'UID de l'étudiant
         CollectionReference studentSubCollection =
             etudiantsCollection.doc(doc.id).collection(credential.uid);
 
-        // Vérifier si la sous-collection contient des documents
         QuerySnapshot subCollectionSnapshot = await studentSubCollection.get();
         if (subCollectionSnapshot.docs.isNotEmpty) {
-          // Si la sous-collection contient des documents, récupérer les données
           DocumentSnapshot studentDoc =
               await studentSubCollection.doc(credential.uid).get();
           if (studentDoc.exists) {
             setState(() {
               studentsData.add(studentDoc.data() as Map<String, dynamic>);
-              //nom = studentDoc['full_name'] ?? '';
               emailUser = studentDoc['Email'] ?? '';
-              //fillier = studentDoc['filiére'] ?? '';
-              // _fullNameController.text = studentDoc['full_name'] ?? '';
-              // _phoneNumberController.text = studentDoc['tel'] ?? '';
             });
             print("Data fetched successfully");
+            await getData(); // Appeler getData après avoir défini emailUser
           }
         }
       }
     } catch (e) {
       print('Erreur lors de la récupération des données: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text('Reclamation Envoié'),
+        centerTitle: true,
+      ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : datas.isEmpty
+              ? const Center(child: Text('Aucune réclamation trouvée'))
+              : ListView.builder(
+                  itemCount: datas.length,
+                  itemBuilder: (context, i) {
+                    return Padding(
+                      padding:
+                          const EdgeInsets.only(top: 5, right: 10, left: 10),
+                      child: Card(
+                        child: ListTile(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text("Détails de la réclamation"),
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '${datas[i]['details']}',
+                                        style: TextStyle(
+                                            fontSize: 17,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      child: Text("Fermer"),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                          leading: Text((i + 1).toString()),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(datas[i]['full_name']),
+                                  Text('Tu as posté une réclamation'),
+                                ],
+                              ),
+                              Text('Semestre réclamé : ${datas[i]['semester']}'),
+                              Text('Nom de la matière : ${datas[i]['nomMatiere']}'),
+                              Text('Note exacte : ${datas[i]['noteExact']}'),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(child: Text('Envoyée le : ')),
+                                  Expanded(child: Text(datas[i]['dateEnvoie'])),
+                                ],
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(6),
+                                decoration: BoxDecoration(
+                                  color: _getEtatColor(datas[i]['etat']),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                child: Text('La réclamation est : ${datas[i]['etat']}'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+    );
+  }
+
+  Color _getEtatColor(String etat) {
+    switch (etat) {
+      case 'Envoyée':
+        return Colors.grey;
+      case 'Refusée':
+        return Colors.redAccent;
+      case 'Acceptée':
+        return Color.fromARGB(255, 134, 255, 82);
+      case 'Révisée':
+        return Colors.blueAccent;
+      default:
+        return Colors.grey;
     }
   }
 }
